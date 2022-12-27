b1642569f60c5c84d227173403dd1dff292f0434
sugi1030 <zhidaole407@gmail.com>
2022-12-27 14:10:17 +0900

    musicaaaaaaap
    musiccccc

---
+++ js/event.js
@@ -0,0 +1,22 @@
+const musicList=[
+    {
+        name:"Alone", artist:"color Out", img:"album1", audio:"Alone"
+    },
+    {
+        name:"Mahidevran", artist:"Maze of sorrow", img:"album2", audio:"No_Rest_Or_Endless_Rest"
+    },
+    {
+        name:"No Rest Or Endless Rest", artist:"Lisofv", img:"album3", audio:"No_Rest_Or_Endless_Rest"
+    },
+    {
+        name:"Old Melody", artist:"TAB", img:"album4", audio:"Old_Money"
+    },
+    {
+        name:"Tantalizing Youth", artist:"Social Square", img:"album5", audio:"Tantalizing_Youth"
+    },
+    {
+        name:"The Deep", artist:"Anitek", img:"album6", audio:"The_Deep"
+    }
+    ]
+
+
